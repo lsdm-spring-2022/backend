5fc7a3e117e0d4e076c2556524e4564b0e2003a9
Rafael Rodriguez <rrodriguez1989@gmail.com>
2022-04-08 22:07:33 -0500

    Setup Dockerfile. Added initial logic to retrieve twitter data. Updated requirements.txt.

--- app/services/data_service.py
+++ app/services/data_service.py
@@ -9,13 +9,29 @@ def get_social_media_data(region: str, start_date: str, end_date: str, reddit: s
     logging.debug(f'Getting social media data for region {region}, start {start_date}, end {end_date}, reddit '
                   f'{reddit_bool}, twitter {twitter_bool}')
     reddit_data = []
-    reddit_db_data = None
+    twitter_data = []
     if reddit_bool:
-        reddit_db_data = get_reddit_data_by_region_and_date(region, start_date, end_date)
-    if reddit_db_data is not None:
-        for item in reddit_db_data:
+        reddit_data = get_reddit_data(region, start_date, end_date)
+    if twitter_bool:
+        twitter_data = get_twitter_data(region, start_date, end_date)
+    return reddit_data, twitter_data
+
+
+def get_reddit_data(region: str, start_date: str, end_date: str):
+    reddit_data = []
+    db_data = get_reddit_data_by_region_and_date(region, start_date, end_date)
+    if db_data is not None:
+        for item in db_data:
             data_dict = {'datePosted': str(item.date_posted), 'region': item.region, 'subreddit': item.subreddit,
                          'postTitle': item.post_title, 'upvotes': item.upvotes, 'dateStored': str(item.date_stored),
                          'comments': item.comments}
             reddit_data.append(data_dict)
-    return reddit_data, []
+    return reddit_data
+
+
+def get_twitter_data(region: str, start_date: str, end_date: str):
+    twitter_data = []
+    db_data = []
+    if db_data is not None:
+        twitter_data = []
+    return twitter_data
